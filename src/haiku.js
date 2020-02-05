25f37ff820c5e6a06acb6f72de9c4e49c4fc9c46
Student <example@example.com>
2020-02-05 10:29:39 -0800

    refactor first three tests

--- src/haiku.js
+++ src/haiku.js
@@ -4,17 +4,17 @@ export function Haiku(line1, line2, line3) {
   this.line3 = line3
 };
   Haiku.prototype.checkInput1 = function() {
-    if (this.line1 === "hello") {
+    if (this.line1.includes("")) {
       return true;
     }
   }
   Haiku.prototype.checkInput2 = function(){
-    if (this.line2 === "hola") {
+    if (this.line2.includes("")) {
       return true;
   }
 }
   Haiku.prototype.checkInput3 = function(){
-    if (this.line3 === "bojour") {
+    if (this.line3.includes("")) {
       return true;
   }
 }
